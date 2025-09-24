7fd42cc891183113824425b884c92e8f2d259b0d
eli <ebmv03@gmail.com>
2025-09-23 23:02:41 -0700

    ayno que feo

---
+++ script.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
+
+function descargarPDF() {
+    const a = document.createElement('a');
+    a.href = "./sample-local-pdf.pdf";
+    a.download = 'sample-local-pdf.pdf';
+    a.click();
+}
+
+const saludos = ["¡Hola!", "¡Hey!", "¡Saludos!", "¡Qué tal!", "¡Bienvenido!"];
+setInterval(() => {
+    const random = Math.floor(Math.random() * saludos.length);
+    document.getElementById('autoSaludo').textContent = saludos[random];
+}, 2000);
